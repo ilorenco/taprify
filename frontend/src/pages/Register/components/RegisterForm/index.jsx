a3fd1e247ae1537dc3afad701dcbe8825aeb6ff2
Igor Henrique <igorhenrique102@gmail.com>
2025-11-22 23:15:34 -0300

    fix: adicionar o campo nome na tela de cadastro

--- frontend/src/pages/Register/components/RegisterForm/index.jsx
+++ frontend/src/pages/Register/components/RegisterForm/index.jsx
@@ -1,5 +1,5 @@
 import { TextInput } from '../../../../components/ui/TextInput';
-import { MailIcon, LockKeyholeIcon } from 'lucide-react';
+import { MailIcon, LockKeyholeIcon, UserIcon } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 export function RegisterForm() {
@@ -7,9 +7,9 @@ export function RegisterForm() {
         <div className="flex flex-col gap-4 w-full">
             <h1 className="text-xl md:text-2xl font-bold text-base-input">Faça seu cadastro</h1>
             <form className="flex flex-col items-center justify-center gap-4">
+            <TextInput placeholder="Nome" icon={UserIcon} />
                 <TextInput placeholder="Email" icon={MailIcon} />
                 <TextInput placeholder="Senha" icon={LockKeyholeIcon} />
-                <TextInput placeholder="Confirmar senha" icon={LockKeyholeIcon} />
                 <button type="submit" className="bg-purple font-bold text-lg md:text-2xl text-base-input rounded-lg p-3 my-4 w-full max-w-xs">
                     CADASTRAR
                 </button>
